80ba87d3f805a6dbf7e6e46b737481933758c639
Chelsea Lee <clee@kcts9.org>
2018-12-05 15:44:14 -0800

    set up gulp build with scss compilation, bootstrap; add images

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,18 @@ const rename = require('gulp-rename');
 const crass = require('gulp-crass');
 const plumber = require('gulp-plumber');
 const browserSync = require('browser-sync');
+const imagemin = require('gulp-imagemin');
 sass.compiler = require('node-sass');
 
 const paths = {
+  nm: './node_modules',
   styles_src: './src/sass',
+  vendor: './src/vendor',
   styles_dest: './dist/css',
-  node_modules: './node_modules',
+  scripts_src: './src/js',
+  scripts_dest: './dist/js',
+  image_src: './src/images',
+  image_dest: './dist/images'
 }
 
 // BrowserSync Configuration
@@ -38,17 +44,40 @@ function clean() {
 
 // compile sass, add vendor prefixes, minify CSS
 function styles() {
-  return gulp.src( paths.styles_src + '/*.scss' )
+  return gulp.src(paths.styles_src + '/*.scss')
     .pipe( plumber() )
-    .pipe( sass().on('error', sass.logError) )
+    .pipe( sass().on( 'error', sass.logError ) )
     .pipe( autoprefixer( 'last 2 versions') )
     .pipe( gulp.dest( paths.styles_dest ) )
     .pipe( plumber() )
     .pipe( crass( { pretty: false, sourceMap: true } ) )
-    .pipe( rename( { suffix: '.min'} ) )
-    .pipe( gulp.dest( paths.styles_dest) )
+    .pipe( rename( { suffix: '.min' } ) )
+    .pipe( gulp.dest( paths.styles_dest ) )
     .pipe( server.stream() );
 }
+
+// vendor
+gulp.task('vendor:scss', function() {
+  return gulp.src([paths.nm + '/bootstrap/scss/**/*'])
+    .pipe( gulp.dest( paths.vendor + '/scss/bootstrap' ) );
+});
+
+gulp.task('vendor:js', function() {
+  return gulp.src([
+    paths.nm + '/bootstrap/js/dist/*',
+    paths.nm + '/jquery/dist/*'
+  ])
+    .pipe( gulp.dest( paths.vendor + '/js' ) );
+});
+
+gulp.task( 'vendor', gulp.parallel( 'vendor:scss', 'vendor:js' ) );
+
+// images
+gulp.task('imagemin', function() {
+  return gulp.src(paths.image_src + '/*')
+    .pipe( imagemin() )
+    .pipe( gulp.dest( paths.image_dest ) );
+});
 
 function watchFiles() {
   gulp.watch( paths.styles_src + '/**/*.scss', styles );
