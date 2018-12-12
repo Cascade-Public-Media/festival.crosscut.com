0ea94a3d584cf1c1a5e0a4edbb29779684913255
Christopher C. Wells <chris@chrxs.net>
2018-12-12 15:18:03 -0800

    Verify required config at boot time.

--- api/subscribe.php
+++ api/subscribe.php
@@ -10,14 +10,24 @@ use Symfony\Component\HttpFoundation\Request;
 $request = Request::createFromGlobals();
 $response = new Response();
 
+$config = new Config('config.json');
+$config_required = [
+  'client.base_uri', 'client.id', 'client.secret',
+  'contact.search',
+  'list_field',
+  'timezone',
+  'user.name', 'user.password', 'user.token',
+];
+foreach ($config_required as $key) {
+  if (empty($config[$key])) {
+    $response->setError('Required API configuration missing.')->send();
+  }
+}
+
 $mail = $request->get('mail');
 if (empty($mail) || !filter_var($mail, FILTER_VALIDATE_EMAIL)) {
   $response->setError('Invalid or empty email address.')->send();
 }
-
-$config = new Config('config.json');
-
-// TODO: Confirm required config.
 
 $client = new Client(
   $config->get('client.base_uri'),
