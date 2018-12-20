eda9d8766ceea3f5a8336474d3981fddf8e95512
jliu <liujoseph999@gmail.com>
2018-12-19 17:28:36 -0800

    add content and styling to front page

--- src/js/custom/main.js
+++ src/js/custom/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     console.log('working');
 
-    $('.owl-carousel').owlCarousel();
+    $('#panelists-carousel').owlCarousel();
 
     $('a[href^="#"]').on('click', function(e) {
         e.preventDefault();
@@ -13,6 +13,35 @@ $(document).ready(function() {
         }, 1000, function() {
             // add hash to URL
             window.location.hash = hash;
-        })
+        });
+
+        $('a.nav-link[href^="#"]').removeClass('active');
+        $(this).addClass('active');
+    });
+
+    $('#session-carousel').owlCarousel({
+        stagePadding:500,
+        loop: true,
+        center: true,
+        items: 1,
+        margin: 10,
+        nav:true,
+        responsive: {
+            0:{
+                stagePadding: 30
+            },
+            768:{
+                stagePadding: 110
+            },
+            900:{
+                stagePadding: 170
+            },
+            1024:{
+                stagePadding: 200
+            },
+            1400:{
+                stagePadding: 400
+            }
+        }
     })
 });
